005c5ec0eca8dec1beb7b7f8e713b5fc4f32f177
Shoichi Sano <sano-shoichi-wx@ynu.jp>
2020-08-14 23:07:28 +0900

    aaaaa

--- helloworld.cc
+++ helloworld.cc
@@ -3,21 +3,6 @@
 
 int main(){
     
-    int A=2;
-    int i=0;
-    
-    scanf("%d", &A);
-    
-    printf("Input num. is %d.",A);
-    
-    while(A<10000){
-        A=A*2;
-        if(A>10000){
-            break;
-        }
-        i++;
-        printf("%d回目    %d", i, A);
-        
-    }
+    cout << "Hello, World!" << endl;
     
 }
